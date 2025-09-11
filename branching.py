f64dc86efa5d8de6c3f7b489b1ace49c365bd406
NollOliver <NollO27@chcstudent.com>
2025-09-11 13:29:15 -0400

    Create branching.py

---
+++ branching.py
@@ -0,0 +1,7 @@
+x = 10 
+if x % 2 == 0:
+    print(f"{x} is divisible by 2")
+    print("Test")
+else:                                                          
+    print("This is not a part of the if statement")
+    
