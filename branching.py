8496e49fa92a61794fbedf8585f6ec5eadaa90de
NollOliver <NollO27@chcstudent.com>
2025-09-12 14:17:16 -0400

    1

--- branching.py
+++ branching.py
@@ -4,4 +4,7 @@ if x % 2 == 0:
     print("Test")
 else:                                                          
     print("This is not a part of the if statement")
-    
+    print(f"{x} is divisible by 5") 
+    if x > 5 or x > 15:
+        print(f"{x} is greater than 5 or 15") 
+        
