920365f56c5b37ff1567890dc34e6356a4ce41b9
NollOliver <NollO27@chcstudent.com>
2025-10-24 08:59:27 -0400

    a

--- prob3.py
+++ prob3.py
@@ -1,5 +1,7 @@
 word= ("what is your word?").strip() .lower() 
-if (input):
-    print("It's a palindrome!")
-else:
-    print("It's not a palindrome.")
+print(word) 
+for char in word:
+    (print word(0))
+    reverse = word +char 
+    if word== reverse
+    
