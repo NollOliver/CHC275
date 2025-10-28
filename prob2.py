5d5060b5a558f05873be604d765ea243362a0ba0
NollOliver <NollO27@chcstudent.com>
2025-10-28 08:51:30 -0400

    commit

---
+++ prob2.py
@@ -0,0 +1,31 @@
+small = []
+medium = []
+large = []
+
+check = False
+while check == False:
+   
+    option = input("What is the size of your mushroom?").strip().lower()
+    print(option)
+
+    if option == "stop":
+            check = True
+           
+    if option.isnumeric():
+        option = int(option)
+       
+    if option <= 100:
+        small.append
+        print("You have a small Mushroom")
+       
+    elif option > 100 and option < 200:
+        medium.append
+        print("You have a medium Mushroom")
+           
+    elif option >= 200:
+        large.append
+        print("You have a large Mushroom")
+           
+    else:
+        print("That is not a number!")
+
--- prob3.py
+++ prob3.py
@@ -1,7 +1,15 @@
-word= ("what is your word?").strip() .lower() 
-print(word) 
-for char in word:
-    (print word(0))
-    reverse = word +char 
-    if word== reverse
-    
+i = 0
+word = input("What is your word?").strip().lower()
+print(word)
+check = True
+while i < (len(word) - 1)//2 and check == True:
+   
+    if word[i] == word[len(word)- 1 - i]:
+        i = i + 1
+       
+   
+    else:
+        check = False
+       
+if check == True:
+    print("Congrats this is a palindrome")
